0b0faefd169366f20926a449b61b4a755488f990
Victor Silveira <victor.freitassilveira-ext@ab-inbev.com>
2024-12-18 11:11:39 -0300

    configure initial routes

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const port = process.env.PORT || 5006
+const port = process.env.PORT || 5006;
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('pages/index')
-})
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
 
 const server = app.listen(port, () => {
-  console.log(`Listening on ${port}`)
-})
+  console.log(`Listening on ${port}`);
+});
 
-process.on('SIGTERM', async () => {
-  console.log('SIGTERM signal received: gracefully shutting down')
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM signal received: gracefully shutting down");
   if (server) {
     server.close(() => {
-      console.log('HTTP server closed')
-    })
+      console.log("HTTP server closed");
+    });
   }
-})
+});
 
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    "postgres://u77jeiqtv7et73:pd2019c6f01ba0ab35f6e444c04278f4ab1cb2e1e53873933d296c9a933619ef0@ccba8a0vn4fb2p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d9bacstqoltq50",
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
   }
-  client.end();
-});
+);
