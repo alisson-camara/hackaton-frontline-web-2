17bdbccf8e7d78c89e28dae1d7a18985ffdd0ce8
Nicollas.Santos-ext <nicollas.santos-ext@ab-inbev.com>
2024-12-18 13:18:42 -0300

    Create some structure with mock

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const port = process.env.PORT || 5006;
 
 const app = express();
 
+// PÁGINAS DE EXEMPLO
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-
+// CORS E O USO DE JSON
 app.use(express.json());
 app.use(cors());
 
@@ -22,29 +23,82 @@ app.get("/", (req, res) => {
 app.post('/create-room', async (req, res) => {
 
   const query = req.query;
-  
-  console.log("req:", req);
+
+  const {room, moderator} = query || {};
+
+  console.log("create-room");
   console.log("query:", query);
+  console.log("room:", room);
+  console.log("moderator:", moderator);
+
+  res.send({ "name": room, "currentTask": "Task 1", "moderator": moderator, "players": [ { "name": "Paula", "point": "?" } ] });
+});
+
+app.post('/remove-player', async (req, res) => {
+
+  const query = req.query;
+
+  const {room, moderator} = query || {};
+
+  console.log("remove-player");
+  console.log("query:", query);
+  console.log("room:", room);
+  console.log("moderator:", moderator);
+
+  res.send({ "name": "SalaDaPaula", "currentTask": "Task 1", "moderator": "Paula", "players": [] });
+});
+
+app.post('/reset-votes', async (req, res) => {
+
+  const query = req.query;
+
+  const {room, moderator} = query || {};
+
+  console.log("reset-votes");
+  console.log("query:", query);
+  console.log("room:", room);
+  console.log("moderator:", moderator);
 
   res.send({ "name": "SalaDaPaula", "currentTask": "Task 1", "moderator": "Paula", "players": [ { "name": "Paula", "point": "?" } ] });
 });
 
+app.post('/sendvote', async (req, res) => {
 
+  const query = req.query;
 
-// app.get('/create-room', async (req, res) => {
+  const {room, moderator} = query || {};
 
-//   res.send({ "name": "SalaDaPaula", "currentTask": "Task 1", "moderator": "Paula", "players": [ { "name": "Paula", "point": "?" } ] });
-// });
+  console.log("sendvote");
+  console.log("query:", query);
+  console.log("room:", room);
+  console.log("moderator:", moderator);
 
-app.use((err, req, resp) =>   {
-  return resp.status(err?.statusCode || 500).json({
+  res.send({ "name": "SalaDaPaula", "currentTask": "Task 1", "moderator": "Paula", "players": [ { "name": "Paula", "point": "3" } ] });
+});
+
+app.post('/join-room', async (req, res) => {
+
+  const query = req.query;
+
+  const {room, moderator} = query || {};
+
+  console.log("join-room");
+  console.log("query:", query);
+  console.log("room:", room);
+  console.log("moderator:", moderator);
+
+  res.send({ "name": "SalaDaPaula", "currentTask": "Task 1", "moderator": "Paula", "players": [ { "name": "Paula", "point": "?" } ] });
+});
+
+// RETORNO DE ERRO (DEFAULT)
+app.use((err, req, res, next) => {
+  res.status(err?.statusCode || 500).send({
     status: "error",
     message: err?.message || "Erro desconhecido", 
   });
 });
 
-// app.use("", routes);
-
+// LEVANTANDO O SERVIDOR
 const server = app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
@@ -60,6 +114,7 @@ process.on("SIGTERM", async () => {
 
 const { Client } = require("pg");
 
+// CONECTANDO AO BANCO DE DADOS
 const client = new Client({
   connectionString:
     "postgres://u77jeiqtv7et73:pd2019c6f01ba0ab35f6e444c04278f4ab1cb2e1e53873933d296c9a933619ef0@ccba8a0vn4fb2p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d9bacstqoltq50",
